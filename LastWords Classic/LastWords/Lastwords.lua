2a66b61f3178a6e13e69d8d0a1293fb9bab349e9
Calidonious <141276027+Calidonious@users.noreply.github.com>
2024-11-13 19:55:15 -0700

    first commit!
    commit...submit...faceit...debaseit...deflateit

---
+++ LastWords Classic/LastWords/Lastwords.lua
@@ -0,0 +1,33 @@
+-- Initialize the addon
+local LastWordsFrame = CreateFrame("Frame")
+local lastWords = "I shall return..."  -- Default message; the user can change this
+
+-- Function to check player health
+local function CheckHealth()
+    local healthPercent = (UnitHealth("player") / UnitHealthMax("player")) * 100
+    if healthPercent <= 1 then
+        -- Send the custom message to chat
+        SendChatMessage(lastWords, "SAY")
+        LastWordsFrame:SetScript("OnUpdate", nil)  -- Stop checking after message is sent
+    end
+end
+
+-- Slash command to set the last words
+SLASH_LASTWORDS1 = "/lastwords"
+SlashCmdList["LASTWORDS"] = function(msg)
+    if msg and msg ~= "" then
+        lastWords = msg
+        print("LastWords message set to: " .. msg)
+    else
+        print("Please enter a message for your LastWords.")
+    end
+end
+
+-- Event to start checking health
+LastWordsFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
+LastWordsFrame:SetScript("OnEvent", function(self, event)
+    if event == "PLAYER_REGEN_DISABLED" then
+        -- Start checking health when combat begins
+        self:SetScript("OnUpdate", CheckHealth)
+    end
+end)
